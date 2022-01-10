95d6e2ce5cb0615feef8b973b9a672b0f4e1149a
A. Belard <angebelard@Gmail.com>
2022-01-10 04:39:22 +0200

    update

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -6,7 +6,6 @@ const AdminNotification = require("../models/adminNotificationModel");
 const Notification = require("../models/notificationsModel");
 const updateBalance = require("./updateBalance");
 const { sendEmail } = require("../utils/sendEmail");
-const req = require("express/lib/request");
 
 let date = new Date();
 Date.prototype.addDays = function(days) {
@@ -15,27 +14,36 @@ Date.prototype.addDays = function(days) {
     return date;
 }
 
+router.get("/user/:id", async (req,res) => {
+    try {
+        let user = await User.findOne({_id: req.params.id})
+        return res.json(user)
+    } catch (error) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
 router.post("/user",async (req,res) => {
     try {
         let {names, phone, country, password, email, username, walletAddress, gender, referredby} = req.body
-    
-        let existUser = await User.findOne({
-            email
-        })
-        if (existUser) {
-            return res.status(400).json({
-                emailExist: true,
-                message: "Email already exists"
-            })
-        }
-    
         let existUsername = await User.findOne({
             username
         })
         if (existUsername) {
             return res.status(400).json({
                 usernameExist: true,
-                message: "Username taken."
+                message: "Username is already taken !!"
+            })
+        }
+
+        let existUser = await User.findOne({
+            email
+        })
+        if (existUser) {
+            return res.status(400).json({
+                emailExist: true,
+                message: "Email already exists !!"
             })
         }
     
@@ -58,7 +66,7 @@ router.post("/user",async (req,res) => {
                                verified: false
                             })
         await newUser.save()
-        .then(async (data) => {
+        .then((data) => {
             let options = {
                 from: process.env.EMAIL_USER,
                 to: email,
@@ -72,19 +80,57 @@ router.post("/user",async (req,res) => {
                             <br>
                         </p>
                         <h4>
-                            Please click the below button for your verification!
+                            Please click the below button for your verification and after login with your credentials. 
                         </h4>
-                        <div style="width: 120px; height:50px; background-color:dodgerblue; border-radius:7px; display:flex;justify-items:center;align-items:center"><a href="https://dailymoneybusiness.herokuapp.com/verified/${data._id}" style="text-decoration: none; padding: 15px 40px; border-radius: 7px; font-weight: bold; background-color: dodgerblue; color: white;"><strong>APPROVE</strong></a></div>
+                        <div style="width: 120px; height:50px; background-color:dodgerblue; border-radius:7px; display:flex;justify-items:center;align-items:center">
+                            <a href="http://localhost:5000/verified/${data._id}" style="text-decoration: none; padding: 15px 40px; border-radius: 7px; font-weight: bold; background-color: dodgerblue; color: white;">
+                                <strong>APPROVE</strong>
+                            </a>
+                        </div>
                     </div>
                 `
             }
             sendEmail(options);
-            res.json("User registerd successfully")
+            res.json(data)
         })
         .catch(err => {
             console.log(err)
             res.status(400).json(err)
         })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+router.post("/verify", async (req,res) => {
+    try {
+        let {email, id, username} = req.body
+        let options = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Dailymoney verification",
+            text: `    
+                <div style="font-family: 'Trebuchet MS'">
+                    <h2 style="color: dodgerblue">Hello ${username}👋</h2>
+                    <p>
+                        Thanks for joining <strong>Dailymoney</strong><br> 
+                        now you remain only one step for confirming membership.
+                        <br>
+                    </p>
+                    <h4>
+                        Please click the below button for your verification and after login with your credentials. 
+                    </h4>
+                    <div style="width: 120px; height:50px; background-color:dodgerblue; border-radius:7px; display:flex;justify-items:center;align-items:center">
+                        <a href="http://localhost:5000/verified/${id}" style="text-decoration: none; padding: 15px 40px; border-radius: 7px; font-weight: bold; background-color: dodgerblue; color: white;">
+                            <strong>APPROVE</strong>
+                        </a>
+                    </div>
+                </div>
+            `
+        } 
+        sendEmail(options)
+        return res.json("Sent")
     } catch (error) {
         console.log(error)
         res.status(500).json(error)
@@ -121,11 +167,16 @@ router.get("/verified/:id", async (req,res) => {
                 newNotification.save()
                 .then(()=>{}, err => {throw err})
             }
-            return res.redirect("https://google.com")
+            return res.redirect("http://localhost:3000/login")
     } catch (error) {
         console.log(error)
         res.status(500).json(error)
     }
+})
+
+router.get("/username/:id", async (req,res) => {
+    let user = await User.findOne({_id: String(req.params.id)})
+    return res.json(String(user.username))
 })
 
 router.post("/login", async (req, res) => {
@@ -144,19 +195,21 @@ router.post("/login", async (req, res) => {
         else if (result.verified !== true) {
             return res.status(400).json({
                 success: false,
-                message: "Verify your email in order to login"
+                message: "Verify your email in order to login!",
+                id: result._id,
+                email: result.email
             })
         } else {
             const checkPassword = await bcrypt.compare(password, result.password)
             if (!checkPassword) {
                 return res.status(400).json({
                     success: false,
-                    message: `Incorrect email or password`
+                    message: `Incorrect username or password!`
                 })
             } else {
                 let {_id, names, username, email, gender, balance, walletAddress, totWithdrew, totDeposited, country, phone, referredby} = result;
                 const token = jwt.sign({
-                    _id, names, email, gender, username, balance, walletAddress, totDeposited, totWithdrew, phone, country, referredby
+                    _id, names, username
                 }, process.env.app_private_key,{expiresIn: "1 days"});
 
                 res.status(200).json({
@@ -182,6 +235,16 @@ router.delete("/users",async (req,res) => {
     await User.deleteMany({})
     .then(() => res.json("All deleted"))
     .catch((err) => res.status(400).json(err))
+})
+
+router.get("/referrals/:id", async (req,res) => {
+    try {
+        let referrals = await User.find({referredby: req.params.id}, {username: 1, createdAt: 1})
+        return res.json(referrals)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router
