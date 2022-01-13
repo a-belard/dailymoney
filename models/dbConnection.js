8387ee907a8f55ff35ed5dd5d99b3ed57fcb5af7
A. Belard <angebelard@Gmail.com>
2022-01-13 21:00:40 +0200

    Update dbConnection.js

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 //process.env.mongo_URI || 
-const URI = "mongodb://localhost/dailymoney"
+const URI = process.env.mongo_URI 
+//"mongodb://localhost/dailymoney"
 
 mongoose.connect(URI, {
     keepAlive: true,
