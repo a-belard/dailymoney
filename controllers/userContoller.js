6e6d343689941fc60f86339548871a3839442e6e
A. Belard <angebelard@Gmail.com>
2022-01-14 21:54:43 +0200

    Update userContoller.js

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -267,7 +267,7 @@ router.post("/login", async (req, res) => {
 })
 
 router.get("/users",async (req,res) => {
-    await User.find()   
+    await User.find({verified: true})   
     .then(data => res.json(data))
     .catch(err => res.status(400).json(err))
 })
